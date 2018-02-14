cc5e099ab4b354c569f73d2f3ce86eb8c249764c
marwolaethblack <a.unal677@gmail.com>
2018-02-14 13:40:29 +0100

    add preload scene

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour {
+
+    public static ulong jewels { get; set; }
+    public static float jewelsPerSecond { get; set; }
+    public static List<GameObject> buildings { get; set; }
+    public static List<GameObject> upgrades { get; set; }
+
+    
+    
+
+	// Use this for initialization
+	void Awake () {
+        jewels = 0;
+        jewelsPerSecond = 0;
+        buildings = new List<GameObject>();
+        upgrades = new List<GameObject>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
