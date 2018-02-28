61b3567cb685acfa9132ed9db591fca626248d46
Shadify <boomkin1234@hotmail.com>
2018-02-28 13:09:08 +0100

    Ádd responsive dropdown menu - buildings

---
+++ Assets/Scripts/GenerateButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateButtons : MonoBehaviour
+{
+
+    public Transform _panelGameObject;
+
+    public GameObject buttonPrefab;
+	// Use this for initialization
+	void Start () {
+	    foreach (var building in State.buildings)
+	    {
+	        GameObject button = Instantiate(buttonPrefab);
+	        button.name = building.name;
+            button.GetComponentInChildren<Text>().text = building.name;
+	        button.tag = "Building";
+	        button.transform.SetParent(_panelGameObject);
+	        button.transform.localScale = new Vector3(1f,1f,1f);
+	        button.transform.localPosition = new Vector3(button.transform.position.x, button.transform.position.y, 0);
+	    }
+	}
+}
