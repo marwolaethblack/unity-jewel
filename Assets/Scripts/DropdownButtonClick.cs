d91bd5cc9f8b9ebd4335a626c583be70b0a5e1f5
Shadify <boomkin1234@hotmail.com>
2018-02-28 11:31:01 +0100

    Add dropdown menu

---
+++ Assets/Scripts/DropdownButtonClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownButtonClick : MonoBehaviour
+{
+    private bool _isActive { get; set; }
+    public GameObject _buildingsPanel;
+    private Button _buildingsButton { get; set; }
+
+    // Use this for initialization
+    void Start ()
+    {
+        _isActive = false;
+        _buildingsButton = GameObject.Find("BuildingsButton").GetComponent<Button>();
+        _buildingsButton.onClick.AddListener(ExpandList);
+	}
+
+    void ExpandList()
+    {
+        if (_isActive == false)
+        {
+            _buildingsPanel.SetActive(true);
+            _isActive = true;
+        }
+        else
+        {
+            _buildingsPanel.SetActive(false);
+            _isActive = false;
+        }
+    }
+}
