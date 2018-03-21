eab9e6c5fe89ef78cf0e84337fb3d89e32dfdb5c
marwolaethblack <a.unal677@gmail.com>
2018-03-21 10:53:44 +0100

    assets

---
+++ Assets/AnimateJewelChip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateJewelChip : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject chip;
+
+
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
